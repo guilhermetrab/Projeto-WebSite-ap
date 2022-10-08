80b02294ae67b0293430e49c61c33fb748e1f9a1
Guilherme <108558971+guilhermetrab@users.noreply.github.com>
2022-10-08 19:57:31 -0300

    botao0

--- script.js
+++ script.js
@@ -2,3 +2,19 @@ function mudafoto(foto){
     document.getElementById("icone").src = foto;
 
 }
+
+function tela(){
+    if(window.innerWidth >= 768) {
+        menu.style.display = 'none'
+    } else{
+        menu.style.display = 'block'
+    }
+}
+
+function clickMenu(){
+    if(nav.style.display == 'bloco'){
+        nav.style.display = 'none'
+    } else{
+        nav.style.display = 'block'
+    }
+}
